6ac05ebdd5244b5d78ef9cdb371098db254c7208
Fajar AM <somabangsa@gmail.com>
2022-03-24 21:16:05 +0700

    Build

---
+++ app/generalSettings.js
@@ -0,0 +1,15 @@
+const generalSetting = {
+        
+   
+    "uiSetting": {
+        "defaultTitle": "Kado.in"
+    },
+     
+    "hosts":{
+        "_api": "http://127.0.0.1:5000",
+        "api": "https://kadoin.herokuapp.com",
+        
+    },
+    
+   
+}
